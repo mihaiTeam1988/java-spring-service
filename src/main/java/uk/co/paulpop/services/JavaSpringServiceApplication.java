1aeb9657187692f09bbb726016de1d3d65cf9798
Mihai Anghel <anghelmihai88@yahoo.com>
2018-07-15 05:31:56 +0100

    Test commit 1

--- src/main/java/uk/co/paulpop/services/JavaSpringServiceApplication.java
+++ src/main/java/uk/co/paulpop/services/JavaSpringServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+/**
+ * Test commit
+ */
 @SpringBootApplication
 public class JavaSpringServiceApplication {
 
